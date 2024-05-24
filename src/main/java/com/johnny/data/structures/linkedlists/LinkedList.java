34da8a6d9a050b6f499f7f51907113640167c83b
johnny <john.robertson88@yahoo.com>
2024-05-23 23:19:32 -0700

    Add example for removeFirst item from linkedlist

--- src/main/java/com/johnny/data/structures/linkedlists/LinkedList.java
+++ src/main/java/com/johnny/data/structures/linkedlists/LinkedList.java
@@ -84,4 +84,14 @@ public class LinkedList {
         }
         length++;
     }
+
+    public Node removeFirst(){
+        if(length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if(length == 0) tail = null;
+        return temp;
+    }
 }
--- src/main/java/com/johnny/data/structures/linkedlists/Main.java
+++ src/main/java/com/johnny/data/structures/linkedlists/Main.java
@@ -7,10 +7,14 @@ public class Main {
     }
 
     private static void appendNode() {
+        System.out.println("Appending");
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
         myLinkedList.append(3);
+        myLinkedList.printList();
+        System.out.println();
 
+        System.out.println("Remove Last");
         // Returns Node 3
         System.out.println(myLinkedList.removeLast().value);
         // Returns Node 2
@@ -19,12 +23,22 @@ public class Main {
         System.out.println(myLinkedList.removeLast().value);
         // Returns null
         System.out.println(myLinkedList.removeLast());
+        System.out.println();
 
+        System.out.println("Prepending");
+        myLinkedList.prepend(2);
+        myLinkedList.prepend(3);
         myLinkedList.prepend(4);
-        myLinkedList.prepend(5);
-        myLinkedList.prepend(6);
-
         myLinkedList.printList();
+        System.out.println();
+
+        System.out.println("Remove First");
+        System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.removeFirst());
+        System.out.println();
+        System.out.println();
     }
 
     private static void createNewNode(){
